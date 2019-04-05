eeca9c106d4712b26a50e57dae130d29559524ff
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-04-05 15:24:52 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,1 +1,5 @@
 mvn tomcat8::run
+bat 'cp C:/Program Files (x86)/Jenkins/jobs/testing/branches/master/workspace/target/ROOT.war C:/Program Files/Apache Software Foundation/Tomcat 8.5/webapps'
+cd  C:/Program Files/Apache Software Foundation/Tomcat 8.5/bin
+catalina.bat shutdown
+catalina.bat startup
