72b98bfb582b64894fd502e8a65b043cdb92a75f
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-04-05 17:44:42 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,4 +1,4 @@
-del C:\Tomcat-8.5\webapps\ROOT.war
-copy c:\progra~2\Jenkins\jobs\testing\branches\master\workspace\target\ROOT.war C:\Tomcat-8.5\webapps
+del C:\progra~1\Apache Software Foundation\Tomcat 8.5\webapps\ROOT.war
+copy c:\progra~2\Jenkins\jobs\testing\branches\master\workspace\target\ROOT.war C:\progra~1\Apache Software Foundation\Tomcat 8.5\webapps
 
 
