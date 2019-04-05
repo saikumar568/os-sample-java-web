def07e7aa68e2e4ac470256521b8e4bd659f0e22
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-04-05 16:11:59 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,3 +1,3 @@
 mvn tomcat8::run
-bat 'cp C:/Program Files (x86)/Jenkins/jobs/testing/branches/master/workspace/target/ROOT.war C:/Program Files/Apache Software Foundation/Tomcat 8.5/webapps'
+copy C:/Program Files (x86)/Jenkins/jobs/testing/branches/master/workspace/target/ROOT.war C:/Tomcat 8.5/webapps'
 
