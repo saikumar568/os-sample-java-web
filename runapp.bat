afdfba25515adfe899d5439fd2b6c97cc17131dc
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-07-12 22:20:48 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,1 +1,1 @@
-bat 'copy c:\progra~2\Jenkins\jobs\Ajay\branches\master\workspace\target\ROOT.war C:\progra~1\Apache-Software-Foundation\Tomcat-8.5\webapps'
+#bat 'copy c:\progra~2\Jenkins\jobs\Ajay\branches\master\workspace\target\ROOT.war C:\progra~1\Apache-Software-Foundation\Tomcat-8.5\webapps'
