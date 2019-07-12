1008cb67df4d876c75a89fac2db2075f424072c6
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-07-12 22:29:39 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,1 +1,2 @@
 #bat 'copy c:\progra~2\Jenkins\jobs\Ajay\branches\master\workspace\target\ROOT.war C:\progra~1\Apache-Software-Foundation\Tomcat-8.5\webapps'
+copy "C:\Program Files (x86)\Jenkins\workspace\os-sample-java-web_master\target\ROOT.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps"
